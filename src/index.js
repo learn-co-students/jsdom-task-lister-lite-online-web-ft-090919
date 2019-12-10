b0146a42d5c005ced12d31d81f3512b46cf33b0c
mmman64 <mcnultymarco@gmail.com>
2019-12-10 22:28:15 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // collection of DOM elements to manipulate
+  const domElements = {
+    formInput: document.querySelector('#new-task-description'),
+    submitButton: document.querySelector('input[type=submit]'),
+    taskList: document.getElementById('tasks'),
+    itemDelete: document.querySelector('#far')
+  }
+
+  function createListItem(taskName) {
+    const listItem = document.createElement("li");
+    const listItemContent = document.createTextNode(taskName);
+    listItem.appendChild(listItemContent);
+
+    const trashIcon = document.createElement("i");
+    trashIcon.className += "far fa-trash-alt";
+    listItem.appendChild(trashIcon);
+
+    return listItem;
+  }
+
+  function addTaskToList(taskName) {
+    domElements.taskList.appendChild(createListItem(taskName));
+  }
+
+  // attach event listener to the submit button
+  domElements.submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    addTaskToList(domElements.formInput.value);
+    domElements.formInput.value = "";
+  });
+
+  // attach event listener to the submit button
+  domElements.taskList.addEventListener('click', function(e) {
+    click = e.target;
+    if (click.className == "far fa-trash-alt") {
+      click.parentNode.remove();
+    }
+  });
+
+
 });
