2694edb4c97afdd0f3627e0cc938dbc7c4940d4e
Kenneth Spatta <kenyounot@gmail.com>
2019-12-14 18:53:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const li = document.createElement("LI");
+    const task = document.getElementById('new-task-description').value;
+    const taskUL = document.getElementById('tasks');
+    
+    li.innerHTML = task
+
+    taskUL.appendChild(li);
+  })
 });
