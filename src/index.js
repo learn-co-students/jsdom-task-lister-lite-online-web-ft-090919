e10b5b3b806ad0e5d99d774d885384c7632ffa2a
mmman64 <mcnultymarco@gmail.com>
2019-12-16 14:20:39 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,103 +1,118 @@
-document.addEventListener("DOMContentLoaded", () => {
+// collection of required DOM elements
+const domElements = {
+  formInput: document.querySelector('#new-task-description'),
+  prioritySelection: document.querySelector('#task-priority-select'),
+  submitButton: document.querySelector('input[type=submit]'),
+  taskList: document.getElementById('tasks'),
+  itemDelete: document.querySelector('#far'),
+  sortButtons: document.querySelector('.sort-group')
+}
 
-  // collection of required DOM elements
-  const domElements = {
-    formInput: document.querySelector('#new-task-description'),
-    prioritySelection: document.querySelector('#task-priority-select'),
-    submitButton: document.querySelector('input[type=submit]'),
-    taskList: document.getElementById('tasks'),
-    itemDelete: document.querySelector('#far'),
-    sortButtons: document.querySelector('.sort-group')
+const tasks = [];
+
+function saveTask(taskName) {
+  const select = domElements.prioritySelection;
+  const priorityValue = select[select.selectedIndex].value;
+  const taskObj = {};
+  taskObj[taskName.toLowerCase()] = parseInt(priorityValue);
+  tasks.push(taskObj);
+
+  return taskObj;
+}
+
+// display task with priority highlighting
+function renderTask(taskObj) {
+  const taskName = Object.keys(taskObj)[0];
+  domElements.taskList.appendChild(createListItem(taskName));
+  applyPriorityStyle(taskObj);
+}
+
+// turn user input into a list item
+function createListItem(taskName) {
+  const listItemContent = getListItemContent(taskName);
+  const trashIcon = getTrashIcon();
+  listItemContent.appendChild(trashIcon);
+
+  return listItemContent;
+}
+
+function getListItemContent(taskName) {
+  const listItem = document.createElement("li");
+  const textContainer = document.createElement("span");
+  const listItemText = document.createTextNode(taskName);
+  textContainer.appendChild(listItemText);
+  listItem.appendChild(textContainer);
+
+  return listItem;
+}
+
+function getTrashIcon() {
+  const trashIcon = document.createElement("i");
+  trashIcon.className += "far fa-trash-alt";
+
+  return trashIcon;
+}
+
+function applyPriorityStyle(task) {
+  const priorityLevel = Object.values(task)[0];
+  const priorityStyle = getSelectedPriorityStyle(priorityLevel);
+  const targetListItem = domElements.taskList.lastChild;
+  targetListItem.firstChild.className += priorityStyle;
+}
+
+function getSelectedPriorityStyle(priorityLevel) {
+  switch (priorityLevel) {
+    case 1:
+      priorityStyle = "low";
+      break;
+    case 2:
+      priorityStyle = "medium";
+      break;
+    case 3:
+      priorityStyle = "high";
+      break;
+    default:
+      alert("Something went wrong!");
   }
 
-  let taskArray = [];
+  return priorityStyle;
+}
 
-  // on submit add user input to list and clear text field
-  domElements.submitButton.addEventListener('click', function (e) {
+function resetFields() {
+  domElements.formInput.value = "";
+  domElements.prioritySelection.selectedIndex = 0;
+}
+
+function deleteTask(selection) {
+  tasks.pop();
+  selection.parentNode.remove();
+}
+
+// function sortList(sortingPreference) {
+//   const compare;
+//   if (sortingPreference == "high") {
+//     compare = ()
+//   }
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  domElements.submitButton.addEventListener('click', function(e) {
     e.preventDefault();
-    renderTask(domElements.formInput.value);
-    taskArray.push(domElements.formInput.value);
-    domElements.formInput.value = "";
+    const taskName = domElements.formInput.value;
+    const task = saveTask(taskName);
+    renderTask(task);
+    resetFields();
   });
 
+  // sort task
   domElements.sortButtons.addEventListener('click', function(e) {
     e.target.id == "sort-highest" ? sortList(high) : sortList(low);
   });
 
-  // high = high to low priority, low = low to high priority
-  function sortList(preference) {
-    if (preference == high) {
-      
-    }
-  }
-
-  // turn user input into a list item
-  function createListItem(taskName) {
-    const listItemContent = getListItemContent(taskName);
-    const trashIcon = getTrashIcon();
-    listItemContent.appendChild(trashIcon);
-
-    return listItemContent;
-  }
-
-  function getListItemContent(taskName) {
-    const listItem = document.createElement("li");
-    const textContainer = document.createElement("span");
-    const listItemText = document.createTextNode(taskName);
-    textContainer.appendChild(listItemText);
-    listItem.appendChild(textContainer);
-
-    return listItem;
-  }
-
-  function getTrashIcon() {
-    const trashIcon = document.createElement("i");
-    trashIcon.className += "far fa-trash-alt";
-
-    return trashIcon;
-  }
-
-  function renderTask(taskName) {
-    domElements.taskList.appendChild(createListItem(taskName));
-    applyPriorityStyle();
-  }
-
-  function applyPriorityStyle() {
-    const priorityStyle = getSelectedPriorityStyle();
-    const targetListItem = domElements.taskList.lastChild;
-    targetListItem.firstChild.className += priorityStyle;
-  }
-
-  function getSelectedPriorityStyle() {
-    const selection = domElements.prioritySelection;
-    const value = selection[selection.selectedIndex].value;
-    let priorityStyle = "";
-
-    switch (value) {
-      case 'low_priority':
-        priorityStyle = "low";
-        break;
-      case 'medium_priority':
-        priorityStyle = "medium";
-        break;
-      case 'high_priority':
-        priorityStyle = "high";
-        break;
-      default:
-        alert("Somwething went wrong!");
-    }
-
-    return priorityStyle;
-  }
-
   // delete task
   domElements.taskList.addEventListener('click', function(e) {
-    click = e.target;
-    if (click.className == "far fa-trash-alt") {
-      click.parentNode.remove();
+    if (e.target.className == "far fa-trash-alt") {
+      deleteTask(e.target);
     }
-    taskArray = taskArray.filter(function(item) {
-      return item !== click.previousSibling.textContent
-    });
   });
 });
