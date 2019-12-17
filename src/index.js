dcf67bbdce54a6b972cbffc58db65cc7776ec66e
mmman64 <mcnultymarco@gmail.com>
2019-12-17 13:40:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -5,26 +5,38 @@ const domElements = {
   submitButton: document.querySelector('input[type=submit]'),
   taskList: document.getElementById('tasks'),
   itemDelete: document.querySelector('#far'),
-  sortButtons: document.querySelector('.sort-group')
+  sortButtons: document.querySelector('.sort-group'),
+  resetButton: document.querySelector('#reset'),
+  // does not include the reset button
+  lockableButtons: document.querySelectorAll('.lockable')
 }
 
-const tasks = [];
+let tasks = [];
+// used to preserve tasks in pre-sorted state
+let preservedTasks = [];
+// used to ensure only pre-sorted task state is preserved
+let preserveLock = false;
 
 function saveTask(taskName) {
   const select = domElements.prioritySelection;
   const priorityValue = select[select.selectedIndex].value;
   const taskObj = {};
-  taskObj[taskName.toLowerCase()] = parseInt(priorityValue);
+  taskObj["task"] = taskName;
+  taskObj["priority"] = parseInt(priorityValue);
   tasks.push(taskObj);
 
   return taskObj;
 }
 
 // display task with priority highlighting
-function renderTask(taskObj) {
-  const taskName = Object.keys(taskObj)[0];
-  domElements.taskList.appendChild(createListItem(taskName));
-  applyPriorityStyle(taskObj);
+function renderList(list) {
+  clearList();
+
+  list.forEach(function(taskObj) {
+    const listItem = createListItem(taskObj["task"]);
+    domElements.taskList.appendChild(listItem);
+    applyPriorityStyle(taskObj["priority"]);
+  });
 }
 
 // turn user input into a list item
@@ -36,9 +48,11 @@ function createListItem(taskName) {
   return listItemContent;
 }
 
+// build out the list element
 function getListItemContent(taskName) {
   const listItem = document.createElement("li");
   const textContainer = document.createElement("span");
+  textContainer.setAttribute("contenteditable", "true");
   const listItemText = document.createTextNode(taskName);
   textContainer.appendChild(listItemText);
   listItem.appendChild(textContainer);
@@ -49,17 +63,18 @@ function getListItemContent(taskName) {
 function getTrashIcon() {
   const trashIcon = document.createElement("i");
   trashIcon.className += "far fa-trash-alt";
+  trashIcon.className += " delete";
 
   return trashIcon;
 }
 
-function applyPriorityStyle(task) {
-  const priorityLevel = Object.values(task)[0];
+function applyPriorityStyle(priorityLevel) {
   const priorityStyle = getSelectedPriorityStyle(priorityLevel);
   const targetListItem = domElements.taskList.lastChild;
   targetListItem.firstChild.className += priorityStyle;
 }
 
+// return css class to apply based on priorityLevel
 function getSelectedPriorityStyle(priorityLevel) {
   switch (priorityLevel) {
     case 1:
@@ -88,30 +103,93 @@ function deleteTask(selection) {
   selection.parentNode.remove();
 }
 
-// function sortList(sortingPreference) {
-//   const compare;
-//   if (sortingPreference == "high") {
-//     compare = ()
-//   }
-// }
+function renderSortedList(sortingPreference) {
+  sortList(sortingPreference);
+  renderList(tasks);
+}
+
+function sortList(sortingPreference) {
+  if (preserveLock == false) {
+    // deep copy tasks array to be used when sorting is reset
+    preservedTasks = _.map(tasks, _.clonedeep);
+    // ensure only intended copy is made
+    preserveLock = !preserveLock;
+  }
+  // sort high-to-low and vice-versa
+  if (sortingPreference == "high") {
+    tasks.sort((a, b) => (a.priority < b.priority) ? 1 : -1);
+  } else {
+    tasks.sort((a, b) => (a.priority > b.priority) ? 1 : -1);
+  }
+}
+
+function clearList() {
+  const listItems = document.querySelectorAll("#tasks li");
+
+  for (let i = 0; li = listItems[i]; i++) {
+    li.parentNode.removeChild(li);
+  }
+}
+
+// toggle disabled status of reset button
+function toggleListReset(buttonClass) {
+  if (buttonClass == "sort-button disabled") {
+    domElements.resetButton.classList.toggle("disabled");
+  }
+}
+
+// prevent user from adding to list while in sorted dtate
+function toggleInputState(state) {
+  const lockableButtons = domElements.lockableButtons;
+  for (const button of lockableButtons) {
+    if (state == "lock" && button.className == "lockable disabled") {
+      return;
+    } else if (state == "lock" && button.className == "lockable") {
+      button.className += " disabled";
+    } else if (state == "unlock" && button.className == "lockable disabled") {
+      button.className = "lockable";
+    }
+  }
+}
+
+// render the list according to user input
+function handleSortSelection(input) {
+  if (input.value == "sort-highest") {
+    renderSortedList("high");
+    toggleListReset(domElements.resetButton.className);
+    toggleInputState("lock");
+  } else {
+    renderSortedList("low");
+    toggleListReset(domElements.resetButton.className);
+    toggleInputState("lock");
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   domElements.submitButton.addEventListener('click', function(e) {
-    e.preventDefault();
     const taskName = domElements.formInput.value;
-    const task = saveTask(taskName);
-    renderTask(task);
+
+    e.preventDefault();
+    saveTask(taskName);
+    clearList();
+    renderList(tasks);
     resetFields();
   });
 
   // sort task
-  domElements.sortButtons.addEventListener('click', function(e) {
-    e.target.id == "sort-highest" ? sortList(high) : sortList(low);
+  domElements.resetButton.addEventListener('click', function(e) {
+    // resume with preservedTasks state
+    tasks = _.map(preservedTasks, _.clonedeep);
+    domElements.resetButton.classList += " disabled";
+    // preserveLock is set to false
+    preserveLock = !preserveLock;
+    toggleInputState("unlock");
+    renderList(tasks);
   });
 
   // delete task
   domElements.taskList.addEventListener('click', function(e) {
-    if (e.target.className == "far fa-trash-alt") {
+    if (e.target.className == "far fa-trash-alt delete") {
       deleteTask(e.target);
     }
   });
