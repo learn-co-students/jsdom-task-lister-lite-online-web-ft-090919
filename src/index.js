6e8e3d7163d4720296fa404cbe4f10ccbea86fd1
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-12-18 17:07:14 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submitButton = document.querySelector("#submit")
+  const textBox = document.querySelector("#new-task-description")
+  const toDoList = document.querySelector("#tasks")
+
+  console.log("loaded")
+
+  submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("event triggered")
+    addToDo(textBox.value)
+    textBox.value = ""
+  })
+
+  function addToDo(string) {
+    let liElement = document.createElement("li")
+    liElement.innerHTML = string
+    console.log(liElement)
+    toDoList.appendChild(liElement)
+    console.log("li should be added")
+  }
+  
 });
+
