9091c0b8da8feb02e3157bcc99b1709c8f54b968
clonex10100 <clonex10100@gmail.com>
2020-02-20 21:11:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    let ul = document.getElementById('tasks');
+    let text = document.getElementById('new-task-description');
+    document.getElementById('create-task-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        let li = document.createElement('li');
+        li.innerText = text.value;
+        text.value = '';
+        ul.appendChild(li);
+    });
 });
